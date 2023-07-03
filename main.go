06ce7e2c8e89855facb58270ba7d2b3b0280dfa2
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2023-07-03 22:31:59 +0900

    ch10.2 implement todo app

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"embed"
+	"errors"
+	"html/template"
+	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -16,6 +21,58 @@ import (
 	_ "github.com/lib/pq"
 )
 
+//go:embed static
+var static embed.FS
+
+//go:embed templates
+var templates embed.FS
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func formatDateTime(d time.Time) string {
+	if d.IsZero() {
+		return ""
+	}
+	return d.Format("2006-01-02 15:04")
+}
+
+type Todo struct {
+	bun.BaseModel `bun:"table:todos,alias:t"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Content   string    `bun:"content,notnull"`
+	Done      bool      `bun:"done"`
+	Until     time.Time `bun:"until,nullzero"`
+	CreatedAt time.Time
+	UpdatedAt time.Time `bun:",nullzero"`
+	DeletedAt time.Time `bun:",soft_delete,nullzero"`
+}
+
+type Data struct {
+	Todos  []Todo
+	Errors []error
+}
+
+func bindUntil(todo *Todo) func([]string) []error {
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{echo.NewBindingError("until", values[0:1], "failed to decode time", err)}
+		}
+		todo.Until = dt
+		return nil
+	}
+}
+
 func main() {
 	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
@@ -27,7 +84,7 @@ func main() {
 	defer db.Close()
 
 	db.AddQueryHook(bundebug.NewQueryHook(
-		// bundebug.WithVerbose(true),
+		bundebug.WithVerbose(true),
 		bundebug.FromEnv("BUNDEBUG"),
 	))
 
@@ -38,20 +95,84 @@ func main() {
 	}
 
 	e := echo.New()
+
+	e.Renderer = &Template{
+		templates: template.Must(template.New("").
+			Funcs(template.FuncMap{
+				"FormatDateTime": formatDateTime,
+			}).ParseFS(templates, "templates/*")),
+	}
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Let's write down TODO list!")
+		var todos []Todo
+		ctx := context.Background()
+		err := db.NewSelect().Model(&todos).Order("created_at").Scan(ctx)
+		if err != nil {
+			e.Logger.Error(err)
+			return c.Render(http.StatusBadRequest, "index", Data{
+				Errors: []error{errors.New("Cannot get todos")},
+			})
+		}
+		return c.Render(http.StatusOK, "index", Data{Todos: todos})
 	})
+
+	e.POST("/", func(c echo.Context) error {
+		var todo Todo
+		// bind form params to todo's fields
+		errs := echo.FormFieldBinder(c).
+			Int64("id", &todo.ID).
+			String("content", &todo.Content).
+			Bool("done", &todo.Done).
+			CustomFunc("until", bindUntil(&todo)).
+			BindErrors()
+		if errs != nil {
+			e.Logger.Error(errs)
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+		} else if todo.ID == 0 {
+			// register a ToDo task
+			ctx := context.Background()
+			if todo.Content == "" {
+				err = errors.New("Todo not found")
+			} else {
+				todo.CreatedAt = time.Now()
+				_, err = db.NewInsert().Model(&todo).Exec(ctx)
+				if err != nil {
+					e.Logger.Error(err)
+					err = errors.New("Cannot update")
+				}
+			}
+		} else {
+			ctx := context.Background()
+			if c.FormValue("delete") != "" {
+				// delete a ToDo task
+				_, err = db.NewDelete().Model(&todo).Where("id = ?", todo.ID).Exec(ctx)
+			} else {
+				// update a ToDo task
+				var original Todo
+				err = db.NewSelect().Model(&original).Where("id = ?", todo.ID).Scan(ctx)
+				if err == nil {
+					original.Done = todo.Done
+					original.UpdatedAt = time.Now()
+					_, err = db.NewUpdate().Model(&original).Where("id = ?", todo.ID).Exec(ctx)
+				}
+			}
+			if err != nil {
+				e.Logger.Error(err)
+				err = errors.New("Cannot update")
+			}
+		}
+		if err != nil {
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
+		}
+		return c.Redirect(http.StatusFound, "/")
+	})
+
+	staticFs, err := fs.Sub(static, "static")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileServer := http.FileServer(http.FileSystem(http.FS(staticFs)))
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", fileServer)))
+
 	e.Logger.Fatal(e.Start(":8989"))
-}
-
-type Todo struct {
-	bun.BaseModel `bun:"todos,alias:t"`
-
-	ID        int64     `bun:"id,pk,autoincrement"`
-	Content   string    `bun:"content,notnull"`
-	Done      bool      `bun:"done"`
-	Until     time.Time `bun:"until,nullzero"`
-	CreatedAt time.Time
-	UpdatedAt time.Time `bun:",nullzero"`
-	DeletedAt time.Time `bun:",soft_delete,nullzero"`
 }
